c2ffd16dc4c7d5545545c50e7f9ccc6972a63f68
mpinheiro <mpinheiro@orion.tech>
2020-08-04 14:37:38 -0300

    first commit

---
+++ src/main/java/com/example/banzinga/BanzingaApplication.java
@@ -0,0 +1,13 @@
+package com.example.banzinga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanzingaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanzingaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/banzinga/BanzingaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banzinga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanzingaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
