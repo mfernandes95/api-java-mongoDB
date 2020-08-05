390c729dfa35ce3f558d8e81c1d81cc3af1d46b8
mpinheiro <mpinheiro@orion.tech>
2020-08-05 01:47:28 -0300

    feat POST User

--- src/main/java/com/example/banzinga/resources/UserResource.java
+++ src/main/java/com/example/banzinga/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.example.banzinga.dto.UserDTO;
 import com.example.banzinga.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,12 +23,9 @@ public class UserResource {
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
-//        User maria = new User("1", "Matia Silva", "maria@gmail.com");
-//        User alex = new User("2", "Alex Silva", "alex@gmail.com");
 
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-//        list.addAll(Arrays.asList(maria, alex));
 
         return ResponseEntity.ok().body(listDto);
     }
@@ -40,9 +36,19 @@ public class UserResource {
         User obj = service.findById(id);
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-//        list.addAll(Arrays.asList(maria, alex));
 
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/example/banzinga/services/UserService.java
+++ src/main/java/com/example/banzinga/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.banzinga.services;
 
 import com.example.banzinga.domain.User;
+import com.example.banzinga.dto.UserDTO;
 import com.example.banzinga.repository.UserRepository;
 import com.example.banzinga.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
