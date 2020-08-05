b4461cb9bc51bccbf710da900303c118e67cf335
mpinheiro <mpinheiro@orion.tech>
2020-08-04 21:33:05 -0300

    feat User repository/ config mongodb connection

--- src/main/java/com/example/banzinga/domain/User.java
+++ src/main/java/com/example/banzinga/domain/User.java
@@ -1,11 +1,17 @@
 package com.example.banzinga.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
+import java.lang.annotation.Documented;
 import java.util.Objects;
 
+@Document
 public class User implements Serializable {
     private static final long SerialVersionUID = 1L;
 
+    @Id
     private String id;
     private String name;
     private String email;
---
+++ src/main/java/com/example/banzinga/services/UserService.java
@@ -0,0 +1,19 @@
+package com.example.banzinga.services;
+
+import com.example.banzinga.domain.User;
+import com.example.banzinga.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
