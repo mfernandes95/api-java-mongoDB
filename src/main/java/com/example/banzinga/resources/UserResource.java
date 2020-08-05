b22569e4559f37a3a1482ce7c2bddbd9e2b65185
mpinheiro <mpinheiro@orion.tech>
2020-08-04 22:19:09 -0300

    feat user DTO

--- src/main/java/com/example/banzinga/resources/UserResource.java
+++ src/main/java/com/example/banzinga/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.banzinga.resources;
 
 import com.example.banzinga.domain.User;
+import com.example.banzinga.dto.UserDTO;
 import com.example.banzinga.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,13 +22,14 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
 //        User maria = new User("1", "Matia Silva", "maria@gmail.com");
 //        User alex = new User("2", "Alex Silva", "alex@gmail.com");
 
         List<User> list = service.findAll();
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 //        list.addAll(Arrays.asList(maria, alex));
 
-        return ResponseEntity.ok().body(list);
+        return ResponseEntity.ok().body(listDto);
     }
 }
